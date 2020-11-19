fb36272478c569e0c88b88134d94e31068a04d5d
Erick Recher <javascriptforlife@gmail.com>
2020-11-19 15:21:35 -0800

    Rick Roll Complete

--- starter-code/submit/base.js
+++ starter-code/submit/base.js
@@ -3,7 +3,12 @@ console.log("Sanity Check: JS is working!");
 var rick_roll_youtube_embed_code = "<iframe width='420' height='315' src='https://www.youtube.com/embed/dQw4w9WgXcQ' frameborder='0' allowfullscreen></iframe>";
 
 $(document).ready(function(){
-
   $("#time").text( Date.now() );
-
-})
+  // $('form').on("submit", function(event){
+  //   event.preventDefault();
+  $('form').on("submit", false);
+  $('form').on("submit", function(){
+    // $('form').on("submit", false);
+    $("body").append(rick_roll_youtube_embed_code);
+  });;
+});
