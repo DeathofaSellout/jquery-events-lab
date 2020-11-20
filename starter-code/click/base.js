94dac94b0cb8e75b04239f2b458b658dbe588515
Erick Recher <javascriptforlife@gmail.com>
2020-11-19 18:34:14 -0800

    track you

--- starter-code/click/base.js
+++ starter-code/click/base.js
@@ -1,5 +1,9 @@
 console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
-
+  $('span').on("click", function(){
+      let time = event.timeStamp;
+      let listItem = $(this).text();
+      $('ul').append("<li>" + listItem +"</li>" + time);
+  })
 })
