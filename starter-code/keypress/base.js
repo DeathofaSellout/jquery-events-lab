df478a7e40bed51019a3f10f79fa0d9782dad5ae
Erick Recher <javascriptforlife@gmail.com>
2020-11-19 18:53:38 -0800

    timestamps

--- starter-code/keypress/base.js
+++ starter-code/keypress/base.js
@@ -1,5 +1,20 @@
 console.log("Sanity Check: JS is working!");
 
 $(document).ready(function(){
-
+    var start;
+    var end;
+    var keyPresses = 0;
+  $(document).on("keypress", function(){
+      keyPresses += 1;
+      if(keyPresses % 2 === 1){
+        start = event.timeStamp;
+        $("#total-time").text(start + " Hit the spacebar again!");
+      } else {
+        keyPresses - 1;
+        end = event.timeStamp - start;
+          $("#total-time").text("Total time between spacebar hits:" + end);
+          start = 0;
+          end = 0;
+      }
+  });
 })
